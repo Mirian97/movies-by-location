57d41fb037ee8166c29a1208c37302e68099dba7
Mirian Quispe <mirianquispe1505@gmail.com>
2025-07-27 18:58:24 -0300

    Add MovieLocation record to model package

---
+++ src/main/java/quispe/movieLocation/model/MovieLocation.java
@@ -0,0 +1,10 @@
+package quispe.movieLocation.model;
+
+public record MovieLocation(
+		String title,
+		String release_year,
+		String location,
+		String actor_1,
+		String actor_2,
+		String actor_3) {
+}
