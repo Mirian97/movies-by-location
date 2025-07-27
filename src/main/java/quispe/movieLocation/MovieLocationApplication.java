ad37b6b422e5794fd1547e4bb8a6015d37016f59
Mirian Quispe <mirianquispe1505@gmail.com>
2025-07-27 18:58:10 -0300

    Add initial application and test classes for MovieLocation

---
+++ src/main/java/quispe/movieLocation/MovieLocationApplication.java
@@ -0,0 +1,13 @@
+package quispe.movieLocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieLocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/quispe_where_movies/whereMovies/MovieLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package quispe_where_movies.whereMovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
